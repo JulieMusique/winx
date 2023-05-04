5d5ad3b46d178faa61e3bf10fdcebc60bb2ed629
Jusdepommes <juliecatteau@orange.fr>
2023-05-04 17:30:12 +0200

    front début

---
+++ frontend/winx/src/App.js
@@ -0,0 +1,16 @@
+//import { Grid, Typography } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="affiche">
+        <img className="affiche-img" src="./static/images/images.jpg" alt="img"/>
+        <h1>Bienvenue !</h1>
+        <button type="submit">Connecter</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
